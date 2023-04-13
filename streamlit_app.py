b6c0569fe5b3ec80a777d7c46866ba8d6a38f443
Catherine Blatter <catherine.blatter@unibas.ch>
2023-04-13 16:52:24 +0200

    --update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,10 @@
 
 import streamlit as st
 from supabase import create_client, Client
-
 import pandas as pd
 import json
-import numpy as np
 import datetime as dt
 from plotnine import *
-
 
 ### UI set page to wide
 st.set_page_config(layout="wide")
